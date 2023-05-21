3cff6f5e9751382f3d79a94740fcf2e979170a57
buddhi444 <fiatmillionaire@yahoo.com>
2023-05-21 17:59:27 +0530

    first

---
+++ server.js
@@ -0,0 +1,124 @@
+// const PORT = 8000;
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// require('dotenv').config();
+// const fs = require('fs');
+// const multer = require('multer');
+// const upload = multer({ storage: storage }).single('file');
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public');
+//   },
+//   filename: (req, file, cb) => {
+//     console.lof('file', file);
+//     cb(null, Date.now() + '-' + file.originalname);
+//   },
+// });
+
+// const { Configuration, OpenAIApi } = require('openai');
+// const configuration = new Configuration({
+//   apiKey: process.env.API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+// app.post('/images', async (req, res) => {
+//   try {
+//     const response = await openai.createImage({
+//       prompt: req.body.message,
+//       n: 4,
+//       size: '1024x1024',
+//     });
+//     console.log(response.data.data);
+//     res.send(response.data.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
+
+// app.post('/upload', (req, res) => {
+//   upload(req, res, (err) => {
+//     if (err instanceof multer.MulterError) {
+//       return res.status(500).json(err);
+//     } else if (err) {
+//       return res.status(500).json(err);
+//     }
+//     console.log(req.file);
+//   });
+// });
+
+// app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT));
+
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
+const fs = require('fs');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public');
+  },
+  filename: (req, file, cb) => {
+    console.log('file', file);
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage }).single('file');
+
+let filePath;
+
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+  apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.post('/images', async (req, res) => {
+  try {
+    const response = await openai.createImage({
+      prompt: req.body.message,
+      n: 4,
+      size: '1024x1024',
+    });
+    console.log(response.data.data);
+    res.send(response.data.data);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      return res.status(500).json(err);
+    } else if (err) {
+      return res.status(500).json(err);
+    }
+
+    filePath = req.file.path;
+  });
+});
+
+app.post('/variations', async (req, res) => {
+  try {
+    const response = await openai.createImageVariation(
+      fs.createReadStream(filePath),
+      2,
+      '1024x1024'
+    );
+    res.send(response.data.data);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT));
---
+++ src/components/Modal.js
@@ -0,0 +1,43 @@
+import { useState, useRef } from 'react';
+
+const Modal = ({
+  setModalOpen,
+  setSelectedImage,
+  selectedImage,
+  generateVariations,
+}) => {
+  const [error, setError] = useState(null);
+  const ref = useRef(null);
+  console.log('selectedImage', selectedImage);
+  const closeModal = () => {
+    setModalOpen(false);
+    setSelectedImage(null);
+  };
+  const checkSize = () => {
+    if (ref.current.width == 256 && ref.current.height == 256) {
+      generateVariations();
+    } else {
+      setError('Error: choose 256x256 image please');
+    }
+  };
+  return (
+    <div className='modal'>
+      <div onClick={closeModal}>✖</div>
+      <div className='img-container'>
+        {selectedImage && (
+          <img
+            ref={ref}
+            src={URL.createObjectURL(selectedImage)}
+            alt='uploaded image'
+          />
+        )}
+      </div>
+
+      <p>{error || '*Image must be 256 by 256'}</p>
+      {!error && <button onClick={checkSize}>Generate</button>}
+      {error && <button onClick={closeModal}>Close this and try again</button>}
+    </div>
+  );
+};
+
+export default Modal;
